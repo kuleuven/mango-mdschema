9de58d607d34e34bf0eeba0e019ca8a1da5c6cd5
Mariana <mariana.montes@kuleuven.be>
2023-04-26 19:37:22 +0200

    add basic validation of metadata

--- schema.py
+++ schema.py
@@ -1,16 +1,21 @@
 import json
+import logging
 
 from irods.data_object import iRODSDataObject
 from irods.collection import iRODSCollection
 from irods.meta import AVUOperation
 
+import fields
+
 class Schema:
     
-    def __init__(self, path: str):
+    def __init__(self, path: str, prefix: str = 'mgs'):
         """Class representing a Metadata Schema to apply.
 
         Args:
             path (str): Path to the metadata schema.
+            prefix (str): Prefix to add to the metadata names.
+                Default is 'mgs' (ManGO schema).
             
         Attributes:
             name (str): Name of the schema.
@@ -47,26 +52,55 @@ class Schema:
         self.name = schema['schema_name']
         self.version = schema['version']
         self.title = schema['title'] if schema['title'] else self.name
+        self.fields = { k : fields.Field.choose_class(k, v) for k, v in schema['properties'].items()}
+        for subfield in self.fields.values():
+            subfield.flatten_name(f"{prefix}.{self.name}")
+        self.required_fields = {subfield.name : subfield.default for subfield in self.fields.values() if subfield.required}
         
-        # add properties with their particular classes
-        
-    def apply(item: iRODSCollection | iRODSDataObject, metadata: dict, verbose: bool = False):
+    def apply(self, item: iRODSCollection | iRODSDataObject, metadata: dict, verbose: bool = False):
         """Apply a metadata schema to an iRODS data object or collection.
 
         Args:
             item (iRODSCollection | iRODSDataObject): iRODS data object or collection to apply metadata to.
             metadata (dict): Dictionary of metadata to apply.
-            verbose (bool, optional): Whether warning should be printed when non-required fields are missing
-                and when the default value of a required field is used.
+            verbose (bool, optional): Whether warning should be printed when non-required fields are missing,
+                when the default value of a required field is used
+                and when fields are provided that do not belong to the schema.
                 Defaults to False.
                 Warnings will be printed when non-required fields are present but not valid.
         """
         # make sure that the required fields are present
+        missing_required = [field_name for field_name, default in self.required_fields.items()
+                            if not field_name in metadata and default is None]
         ## raise error if any required field is not present and there is no default
-        ## send warnings (if verbose) for missing non-required fields
-        ## send warnings (if verbose) when the default value of a required field is used
+        if len(missing_required) > 1:
+            raise KeyError(f"The following required fields are missing and there is no default: {', '.join(missing_required)}.")
         
+        go_to_default = [field_name for field_name, default in self.required_fields.items()
+                         if not field_name in metadata and default is not None]
+        to_use = {k : v for k, v in metadata if k in self.fields}
+        
+        if verbose:
+            ## send warning when the default value of a required field is used
+            if len(go_to_default) > 1:
+                default_pairs = ', '.join(f"{field_name} ({self.required_fields[field_name]})" for field_name in go_to_default)
+                logging.warning(f"The following required fields are missing and the default value will be used: {default_pairs}")
+            
+            ## send warning for missing non-required fields
+            missing_non_required = [field_name for field_name, field in self.fields.items()
+                                    if not field.required and not field_name in metadata]
+            if len(missing_non_required) > 1:
+                logging.warning(f"The following non required fields are missing: {', '.join(missing_non_required)}.")
+            
+            ## send warnings for irrelevant metadata fields
+            if len(to_use) < len(metadata):
+                extras = ', '.join(x for x in metadata.keys() if not x in self.fields)
+                logging.warning(f"The following fields do not belong to the schema and will be ignored: {extras}.")
+            
+        
+        # NOTE: use to_use, not metadata!
         # check that all (required) fields are valid AND turn them into AVUs with flattened name
+        # do this with the field object itself :)
         # if a required field is not valid print an error
         # if a non-required field is not valid print a warning (always)
         
