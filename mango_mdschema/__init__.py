67d68863b3cb903c5bbcdc7e4ad2166a02c571e2
Ronny Moreas <ronny.moreas@kuleuven.be>
2024-01-27 19:25:58 +0000

    feat: make exception directly importable from package
    Signed-off-by: Ronny Moreas <ronny.moreas@kuleuven.be>

--- mango_mdschema/__init__.py
+++ mango_mdschema/__init__.py
@@ -1,5 +1,6 @@
 """Main module for mango_mdschema package."""
 from .schema import Schema
 from .helpers import check_metadata
+from .exceptions import ConversionError, ValidationError
 
-__all__ = ["Schema", "check_metadata"]
+__all__ = ["Schema", "check_metadata", "ConversionError", "ValidationError"]
